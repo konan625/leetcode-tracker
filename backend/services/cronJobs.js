aa0264629ca93e48141854bf4566443470c4ead9
aanchal625 <82753429+aanchal625@users.noreply.github.com>
2025-07-07 02:49:19 +0530

    final2

--- backend/services/cronJobs.js
+++ backend/services/cronJobs.js
@@ -5,18 +5,18 @@ const sendDailyEmails = require("./emailService");
 
 // Schedule at 11 PM IST every day
 cron.schedule(
-  "0 23 * * *",
-    //  "*/5 * * * *",  // runs every minute
-  async () => {
-    console.log("🕒 Running daily email job...");
-    try {
-      await sendDailyEmails();
-      console.log("✅ All emails dispatched.");
-    } catch (err) {
-      console.error("❌ Cron job error:", err);
-    }
-  },
-  { timezone: "Asia/Kolkata" }
+    //   "0 23 * * *",
+    "*/5 * * * *",  // runs every minute
+    async () => {
+        console.log("🕒 Running daily email job...");
+        try {
+            await sendDailyEmails();
+            console.log("✅ All emails dispatched.");
+        } catch (err) {
+            console.error("❌ Cron job error:", err);
+        }
+    },
+    { timezone: "Asia/Kolkata" }
 );
 
 console.log("⏰ Cron job scheduled for 11 PM daily.");
